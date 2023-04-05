68cc816dba5d7756088f207d16cb1b9587bab9a6
Nhungnguyen217 <63488905+Nhungnguyen217@users.noreply.github.com>
2023-04-05 13:45:23 +0700

    change

--- buildModel.py
+++ buildModel.py
@@ -21,50 +21,18 @@ import numpy as np
 
 # step 2: Loading the data
 labels = ['Speed limit (20km/h)',
-          'Speed limit (30km/h)',
-          'Speed limit (50km/h)',
-          'Speed limit (60km/h)',
-          'Speed limit (70km/h)',
-          'Speed limit (80km/h)',
-          'End of speed limit (80km/h)',
-          'Speed limit (100km/h)',
-          'Speed limit (120km/h)',
           'No passing',
-          'No passing for vechiles over 3.5 metric tons',
           'Right-of-way at the next intersection',
-          'Priority road',
           'Yield',
           'Stop',
-          'No vechiles',
-          'Vechiles over 3.5 metric tons prohibited',
-          'No entry',
-          'General caution',
-          'Dangerous curve to the left',
-          'Dangerous curve to the right',
-          'Double curve',
-          'Bumpy road',
-          'Slippery road',
-          'Road narrows on the right',
-          'Road work',
           'Traffic signals',
-          'Pedestrians',
           'Children crossing',
-          'Bicycles crossing',
-          'Beware of ice/snow',
-          'Wild animals crossing',
-          'End of all speed and passing limits',
-          'Turn right ahead',
-          'Turn left ahead',
           'Ahead only',
-          'Go straight or right',
           'Go straight or left',
           'Keep right',
-          'Keep left',
-          'Roundabout mandatory',
-          'End of no passing',
-          'End of no passing by vechiles over 3.5 metric tons',
           ]
 img_size = 32
+
 
 def get_data(data_dir):
     data = []
@@ -73,7 +41,7 @@ def get_data(data_dir):
         class_num = labels.index(label)
         for img in os.listdir(path):
             try:
-                img_arr = cv2.imread(os.path.join(path, img))[...,::-1]  # convert BGR to RGB format
+                img_arr = cv2.imread(os.path.join(path, img))[..., ::-1]  # convert BGR to RGB format
                 resized_arr = cv2.resize(img_arr, (img_size, img_size))  # Reshaping images to preferred size
                 data.append([resized_arr, class_num])
             except Exception as e:
